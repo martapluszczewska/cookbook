46428c5ef252e1b9255d48dfe34f9a2459ed2e7e
marta.pluszczewska <marta.pluszczewska@student.uj.edu.pl>
2020-06-01 16:54:13 +0200

    ingredients

---
+++ src/Entity/Ingredient.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Tag entity.
+ */
+
+namespace App\Entity;
+
+use DateTimeInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class Ingredient.
+ *
+ * @ORM\Entity(repositoryClass="App\Repository\IngredientRepositoryRepository")
+ * @ORM\Table(name="ingredients")
+ *
+ * @UniqueEntity(fields={"title"})
+ */
+class Ingredient
+{
+    /**
+     * Primary key.
+     *
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * Title.
+     *
+     * @var string
+     *
+     * @ORM\Column(
+     *     type="string",
+     *     length=64,
+     * )
+     *
+     * @Assert\Type(type="string")
+     * @Assert\NotBlank
+     * @Assert\Length(
+     *     min="3",
+     *     max="64",
+     * )
+     */
+    private $title;
+
+    /**
+     * Getter for Id.
+     *
+     * @return int|null Result
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Getter for Title.
+     *
+     * @return string|null Title
+     */
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    /**
+     * Setter for Title.
+     *
+     * @param string $title Title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+}
